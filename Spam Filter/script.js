433614535db76d37fa5858518e8982d804b8c847
MarleneGray <marlenegray666@gmail.com>
2024-05-23 17:46:17 +0100

    added a list of regular expressions

--- Spam Filter/script.js
+++ Spam Filter/script.js
@@ -2,8 +2,11 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const helpRegex = /please help/;
-const isSpam = (msg) => false;
+const helpRegex = /please help|assist me/i;
+
+const denyList = [helpRegex];
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
+
 checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
     alert("Please enter a message.");
