0630eded80852dc78848cfee031353b05555c67c
MarleneGray <marlenegray666@gmail.com>
2024-07-01 23:30:52 +0100

    reduced the cid to its to total value

--- Cash register/script.js
+++ Cash register/script.js
@@ -11,13 +11,6 @@ let cid = [
   ['ONE HUNDRED', 100]
 ];
 
-console.log("cid 1,0"+cid[1][0])
-console.log(cid
-  .reverse().map(el=>  `${el[0]} `))
-  //reduce((el,acc)=> { console.log(``); return acc+": $ \n" + el[[0]]},"iniziale "))
-  console.log("prima del reduce"+cid
-    .reverse().reduce((acc,el)=>  acc+el[0],""))
-
 const currencyNameMap = {
   PENNY: 'Pennies',
   NICKEL: 'Nickels',
@@ -30,25 +23,48 @@ const currencyNameMap = {
   'ONE HUNDRED': 'Hundreds'
 };
 
+const currencyValue = {
+  PENNY: 0.01,
+  NICKEL: 0.05,
+  DIME: 0.10,
+  QUARTER: 0.25,
+  ONE: 1,
+  FIVE: 5,
+  TEN: 10,
+  TWENTY: 20,
+  'ONE HUNDRED': 100 
+}
+
+console.log("cid 1,0"+cid[1][0])
+console.log(cid
+  .reverse().map(el=>  `${el[0]} `))
+  //reduce((el,acc)=> { console.log(``); return acc+": $ \n" + el[[0]]},"iniziale "))
+  console.log("prima del reduce"+cid
+    .reverse().reduce((acc,el)=>  acc+el[0],""))
+
+
+
 const calculate = (price, cid, cash)=>{
 
 
 return (newcid,changeDue, cidStatus)
 }
 const test2 =[0,2,3]
-const cashInput = document.getElementById("cash-input")
+const cashInputElement = document.getElementById("cash-input")
+
 const statusHtml = document.getElementById("status");
 const purchaseBtn = document.getElementById("purchase-btn")
+
+
 const test = document.getElementById("test")
 const cidElement = document.getElementById("cid-element")
 const changeDue =0;
 cidElement.innerHTML = cid
 .reduce((acc,el)=> acc+ `${currencyNameMap[el[0]]}: $ ${+el[1]}<br>`,"")
-//.map(el => currencyNameMap[el[0]])
-//.reverse().reduce((el,acc)=> { console.log(``); return acc+": $ \n" + el[[0]]},"iniziale ")
-purchaseBtn.addEventListener("click",() => {  console.log(`ho submittato e cashInput is ${statusHtml}`)
-  //test.innerHTML ="prova prova prova";}
-  //test.innerHTML = `<div>test test test</div>`;
+purchaseBtn.addEventListener("click",() => { // console.log(`ho submittato e cashInput is ${cashInput.value}`);
+const cashInput = cashInputElement.value;
+const totalCash = cid.reduce( (acc,el)=> el[1]+acc,0).toFixed(2)
+console.log(`total money in the cid${totalCash}`)
   })
 const updateStatus = (cid, status)=>{
 
