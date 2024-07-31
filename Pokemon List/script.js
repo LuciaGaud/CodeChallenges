2768e532ee13e2d0e955d3d7d7f3b5c3129a6c65
MarleneGray <marlenegray666@gmail.com>
2024-07-31 10:25:26 +0100

    the color theme now changes based on the pokemon type

--- Pokemon List/script.js
+++ Pokemon List/script.js
@@ -12,9 +12,17 @@ const specialDefense = document.getElementById('special-defense');
 const speed = document.getElementById('speed');
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
-const button = document.getElementById('search-button')
-const reset =()=>{
-pokemonID.innerText=``
+const button = document.getElementById('search-button');
+const lines = document.querySelectorAll('tr:nth-child(even)');
+lines.forEach(line => {
+    line.style.backgroundColor = '#feac32';
+
+    
+});
+
+
+const resetView =()=>{
+    pokemonID.innerText=``
     pokemonName.innerText =``
     types.innerText=``
     height.innerText=``;
@@ -24,16 +32,25 @@ pokemonID.innerText=``
     defense.innerText=``;
     specialAttack.innerText=``;
     specialDefense.innerText=``;
-        speed.innerText=``;
-        image.innerHTML=``
+    speed.innerText=``;
+    spriteContainer.innerHTML=``;
+    types.innerHTML=``;
+    button.classList =`electric`;
+    lines.forEach(line => {
+        line.style.backgroundColor = '#feac32';
+    });
 }
 const findPokemon = async ()=>{
  try{ 
+    resetView();
     const response = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${searchInput.value.toLowerCase()}`);
     const data = await response.json()
     pokemonID.innerText=`#${data.id}`
     pokemonName.innerText =`${data.name}`
-    types.innerText=`${data.types[0].type.name.toUpperCase()}`
+    console.log(data.types)
+    for (const element of data.types){
+        console.log(element.type.name)
+        types.innerHTML +=`<span class="type ${element.type.name}">${element.type.name.toUpperCase()}</span> `}
     height.innerText=`Height: ${data.height}`;
     weight.innerText=`Weight: ${data.weight}`;
     hp.innerText=`${data.stats[0].base_stat}`;
@@ -41,15 +58,22 @@ const findPokemon = async ()=>{
     defense.innerText=`${data.stats[2].base_stat}`;
     specialAttack.innerText=`${data.stats[3].base_stat}`;
     specialDefense.innerText=`${data.stats[4].base_stat}`;
-        speed.innerText=`${data.stats[5].base_stat}`;
-        console.log(`<img src="${data.sprites.front_default}">`);
-           spriteContainer.innerHTML=`<img src="${data.sprites.front_default}">`;
+    speed.innerText=`${data.stats[5].base_stat}`;
+    spriteContainer.innerHTML=`<img src="${data.sprites.front_default}" id="sprite">`;
+    console.log(data.types[0].type.name)
+    button.classList.add( `${data.types[0].type.name}`);
+    lines.forEach(line => {
+        line.style.backgroundColor = '';
+        line.classList.add(data.types[0].type.name);
+        
+    });
+
 
 
 
   }
   catch (err) {
-    reset();
+    resetView();
     alert('Pokémon not found');
     console.log(`Pokémon not found: ${err}`);
   }
